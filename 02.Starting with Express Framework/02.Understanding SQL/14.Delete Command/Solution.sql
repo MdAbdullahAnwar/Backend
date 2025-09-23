464a52a8bf2a411f5be00c0cff95301d7a91d58f
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-09-24 04:32:36 +0530

    Delete Command

---
+++ 02.Starting with Express Framework/02.Understanding SQL/14.Delete Command/Solution.sql
@@ -0,0 +1,10 @@
+Create table for it
+CREATE TABLE employees (
+    id INT,
+    name VARCHAR(100),
+    age INT,
+    department VARCHAR(100),
+    salary INT
+);
+
+DELETE FROM employees WHERE id = 102;
